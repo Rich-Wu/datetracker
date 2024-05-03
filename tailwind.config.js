0ef7b0b7f1d4a68b26635c84e586937a26567cd1
Richard Wu <Richie1988@gmail.com>
2024-05-03 16:14:26 -0700

    styled forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./templates/**/*.{tmpl,html}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'beat-once': 'beat 0.1s linear 1'
+      },
+      keyframes: {
+        beat: {
+          '0%, 100%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(1.05)' }
+        }
+      }
+    },
   },
   plugins: [],
 }
